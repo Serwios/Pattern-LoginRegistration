641abbd966bf3fc0ad796ebca65d680cc24b59f6
Serwio YK <56473255+Serwios@users.noreply.github.com>
2021-03-25 09:29:54 +0200

    Update AuthProvider.java

--- src/main/java/com/dzeru/springloginformandoauth2tutorial/services/AuthProvider.java
+++ src/main/java/com/dzeru/springloginformandoauth2tutorial/services/AuthProvider.java
@@ -14,38 +14,32 @@ import org.springframework.stereotype.Component;
 import java.util.Collection;
 
 @Component
-public class AuthProvider implements AuthenticationProvider
-{
+public class AuthProvider implements AuthenticationProvider {
 	@Autowired
 	private UserService userService;
 
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 
-	public Authentication authenticate(Authentication authentication) throws AuthenticationException
-	{
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
 		String username = authentication.getName();
 		String password = (String) authentication.getCredentials();
 
 		User user = (User) userService.loadUserByUsername(username);
 
-		if(user != null && (user.getUsername().equals(username) || user.getName().equals(username)))
-		{
-			if(!passwordEncoder.matches(password, user.getPassword()))
-			{
+		if(user != null && (user.getUsername().equals(username) || user.getName().equals(username))) {
+			if(!passwordEncoder.matches(password, user.getPassword())) {
 				throw new BadCredentialsException("Wrong password");
 			}
 
 			Collection<? extends GrantedAuthority> authorities = user.getAuthorities();
 
 			return new UsernamePasswordAuthenticationToken(user, password, authorities);
-		}
-		else
+		} else
 			throw new BadCredentialsException("Username not found");
 	}
 
-	public boolean supports(Class<?> arg)
-	{
+	public boolean supports(Class<?> arg) {
 		return true;
 	}
 }
